ad539192a26122ad2cbcf9906350a1efdb02a668
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-01-17 08:21:16 -0300

    💄 style: Style Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,19 @@
-import { HeaderContainer } from "./styles";
+import left from "../../assets/left-effect.svg";
+import right from "../../assets/right-effect.svg";
+import terminal from "../../assets/terminal.svg";
+import { HeaderContainer, HeaderContent } from "./styles";
 
 export function Header() {
   return (
     <HeaderContainer>
-      <h1>Header</h1>
+      <img src={left} alt="" />
+
+      <HeaderContent>
+        <img src={terminal} alt="" />
+        <h1>GITHUB BLOG</h1>
+      </HeaderContent>
+
+      <img src={right} alt="" />
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,29 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.header``
+export const HeaderContainer = styled.header`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: ${({ theme }) => theme['blue-700']};
+`
+
+export const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 0 auto;
+  gap: 1.25rem;
+  padding: 4rem;
+
+  background-color: ${({ theme }) => theme['blue-700']};
+
+  h1 {
+    color: ${({ theme }) => theme['blue-light']};
+    font: 400 1.5rem 'Coda', cursive;
+    line-height: 1.6;
+    padding-bottom: 4.375rem;
+  }
+`
