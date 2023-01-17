6c686a3ad38f7196cac85cb1906598a7eb0112e8
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-01-17 07:55:16 -0300

    ✨ feat: Add Routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
+import { Router } from "./Routes";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World!!</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyles />
     </ThemeProvider>
   )
