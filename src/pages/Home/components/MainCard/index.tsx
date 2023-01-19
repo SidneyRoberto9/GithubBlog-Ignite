0e5abe2c913b02ecb072cf129b6580a9c5c231ce
SidneyRoberto9 <sidneyrpsilva@gmail.com>
2023-01-19 00:42:28 -0300

    ✨ feat: Populate main card with github api in home page

--- src/pages/Home/components/MainCard/index.tsx
+++ src/pages/Home/components/MainCard/index.tsx
@@ -1,24 +1,53 @@
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faArrowUpRightFromSquare, faBuilding, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 
+import { gitHubUserApi } from "../../../../lib/axios";
 import { Icons, Info, Link, MainCardContainer } from "./styles";
 
+interface GithubAccount {
+  bio: string
+  name: string
+  login: string
+  company: string
+  html_url: string
+  followers: number
+  avatar_url: string
+}
+
 export function MainCard() {
+  const [accountData, setAccountData] = useState<GithubAccount>(
+    {} as GithubAccount,
+  )
+
+  async function getGithubAccount() {
+    const { data } = await gitHubUserApi.get('SidneyRoberto9')
+
+    setAccountData({
+      bio: data.bio,
+      name: data.name,
+      login: data.login,
+      company: data.company,
+      html_url: data.html_url,
+      followers: data.followers,
+      avatar_url: data.avatar_url,
+    })
+  }
+
+  useEffect(() => {
+    getGithubAccount()
+  }, [])
+
   return (
     <MainCardContainer>
-      <img src="https://avatars.githubusercontent.com/u/55926500?v=4" alt="" />
+      <img src={accountData.avatar_url} alt="" />
 
       <Info>
-        <strong>Sidney Roberto</strong>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-          minima delectus debitis ut omnis provident, optio dolores aspernatur
-          ex labore voluptate dicta ea, nam dolorem aut sequi adipisci. Rem,
-          pariatur!
-        </p>
+        <strong>{accountData.name}</strong>
+        <p>{accountData.bio}</p>
 
-        <Link target="_blank" href="https://www.github.com/SidneyRoberto9">
+        <Link target="_blank" href={accountData.html_url}>
           <span>GITHUB</span>
           <FontAwesomeIcon
             icon={faArrowUpRightFromSquare}
@@ -30,17 +59,21 @@ export function MainCard() {
         <Icons>
           <span>
             <FontAwesomeIcon icon={faGithub} />
-            <ins>SidneyRoberto9</ins>
+            <ins>{accountData.login}</ins>
           </span>
 
           <span>
             <FontAwesomeIcon icon={faBuilding} />
-            <ins>RocketSeat</ins>
+            <ins>
+              {accountData.company == null
+                ? 'Sem Companhia'
+                : accountData.company}
+            </ins>
           </span>
 
           <span>
             <FontAwesomeIcon icon={faUserGroup} />
-            <ins> 32 seguidores</ins>
+            <ins> {accountData.followers + ' '} seguidores</ins>
           </span>
         </Icons>
       </Info>
